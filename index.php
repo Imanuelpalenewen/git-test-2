72d77161c7fbd36f39d1fe5dc4ba9bdbefa38404
Wendell06 <imanuelpalenewen15@gmail.com>
2023-09-04 06:14:16 +0800

    Menambahkan paragraft

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Coba Git Remote</title>
+</head>
+<body>
+  <h1>Hello World!</h1>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit corrupti in ea quam cum. Suscipit, sit nobis odio facere itaque distinctio sunt similique, cumque voluptatem necessitatibus quos, unde eveniet reprehenderit!</p>
+</body>
+</html>
