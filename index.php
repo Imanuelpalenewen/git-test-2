1ad6c795f80630146b0e1a6a9a44a1946c93e4c4
Wendell06 <imanuelpalenewen15@gmail.com>
2023-09-04 06:21:46 +0800

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit corrupti in ea quam cum. Suscipit, sit nobis odio facere itaque distinctio sunt similique, cumque voluptatem necessitatibus quos, unde eveniet reprehenderit!</p>
+<ul>
+  <li>satu</li>
+  <li>dua</li>
+</ul>
 </body>
 </html>
